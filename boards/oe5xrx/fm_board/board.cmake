e2f9dc880f4c249f303eae829cab36fe1d10544c
Peter Buchegger <peter.buchegger7@gmail.com>
2026-06-27 11:33:53 +0000

    Port fm_board to STM32U575

--- boards/oe5xrx/fm_board/board.cmake
+++ boards/oe5xrx/fm_board/board.cmake
@@ -2,9 +2,9 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
 # FM Board Runner Configuration
-# Configures flash/debug runners for STM32F302VC target
+# Configures flash/debug runners for STM32U575CIT target
 
-board_runner_args(jlink "--device=STM32F302VC" "--speed=4000")
+board_runner_args(jlink "--device=STM32U575CIT" "--reset-after-load")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
