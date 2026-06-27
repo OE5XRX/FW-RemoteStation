26785881e595ed2fe310d555a6170d869d3a35df
Peter Buchegger <peter.buchegger7@gmail.com>
2026-06-27 21:02:55 +0000

    board: align U575 SA818 wiring and debug tooling

--- boards/oe5xrx/fm_board/board.cmake
+++ boards/oe5xrx/fm_board/board.cmake
@@ -5,6 +5,9 @@
 # Configures flash/debug runners for STM32U575CIT target
 
 board_runner_args(jlink "--device=STM32U575CIT" "--reset-after-load")
+board_runner_args(pyocd "--target=stm32u575citx")
+board_runner_args(pyocd "--flash-opt=-O connect_mode=under-reset")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
